ad3e5050bab39072d75def5c21372595b22eb5dd
Haqverdi Behbudov <haqverdib@gmail.com>
2018-10-20 16:14:10 +0400

    Added notification show time and auto-dismissing
    Added several features like notification show time and auto-dismissing, the script initialization method has been changed, 
    it was =>  simpleNotify.notify ('Hey! This is a warning notification.', ‘warning’); 
    
    became =>  simpleNotify.notify ({
                                 message: 'Hey! This is a warning notification. ',
                                 level: 'good',
                                 notificationTime: 5000,
                                 autoDismissing: false
                             });

--- inc/simpleNotify.js
+++ inc/simpleNotify.js
@@ -5,76 +5,91 @@ A simple notifications library built with pure javascript and no dependancies.
 Copyright Drew Warkentin 2016. All rights reserved.
 
 To do:
--Add a close button to close notifications
--Add support for non auto-dismissing notifications
-
+-Add a close button to close notifications => done
+-Add notification show time => done
+-Add support for non auto-dismissing notifications => done 
 ----------------------------------------------------------------------------- */
 
 var simpleNotify = {
-  // Some important variables
-  NOTIFICATION_CLASS_NAME : "simple-notification",
-  CONTAINER_CLASS_NAME : "simple-notification-container",
-  CONTAINER_ID_NAME : "notificationContainer",
-  MARGIN_BETWEEEN_NOTIFICATIONS : 5, //px
-  NOTIFICATION_WIDTH: 300, //px
-  NOTIFICATION_TIME : 7000, //ms
-  notificationCount : 0,
+	// Some important variables
+	NOTIFICATION_CLASS_NAME: "simple-notification",
+	CONTAINER_CLASS_NAME: "simple-notification-container",
+	CONTAINER_ID_NAME: "notificationContainer",
+	MARGIN_BETWEEEN_NOTIFICATIONS: 5, //px
+	NOTIFICATION_WIDTH: 300, //px
+	NOTIFICATION_TIME: 7000, //ms
+	AUTO_DISMISSING: true,
+	LEVEL: 'good',
+	notificationCount: 0,
 
-  notify : function(message, level) {
-    var level = typeof level !== 'undefined' ?  level : "good";
-    simpleNotify.notificationCount++;
-    var notificationId = 'notification' + simpleNotify.notificationCount;
-    var newNotification = {"id": notificationId, "message": message, "level": level };
-    // If we don't have the notification container already on the page, create it
-     if(document.getElementById("notificationContainer")) {
-       // Show the notification
-       simpleNotify.showNotification(newNotification);
-     } else {
-       // create the container
-       simpleNotify.createContainer();
-       // Show the notification
-       simpleNotify.showNotification(newNotification);
-     }
-  },
+	notify: function (){
+		// arguments
+		this.options = arguments[0] || {};
+		// custom dismissing
+		simpleNotify.AUTO_DISMISSING = typeof this.options.autoDismissing == 'boolean' ? this.options.autoDismissing : true;
+		// custom level
+		simpleNotify.LEVEL = typeof this.options.level !== 'undefined' ? this.options.level : "good";
+		// custom timeout
+		simpleNotify.NOTIFICATION_TIME = (typeof this.options.notificationTime !== 'undefined' && typeof this.options.notificationTime == 'number') ? this.options.notificationTime : 7000;
+		simpleNotify.notificationCount++;
+		var notificationId = 'notification' + simpleNotify.notificationCount;
+		var newNotification = {
+			"id": notificationId,
+			"message": this.options.message,
+			"level": simpleNotify.LEVEL
+		};
+		// If we don't have the notification container already on the page, create it
+		if (document.getElementById("notificationContainer")) {
+			// Show the notification
+			simpleNotify.showNotification(newNotification);
+		} else {
+			// create the container
+			simpleNotify.createContainer();
+			// Show the notification
+			simpleNotify.showNotification(newNotification);
+		}
+	},
 
-  // This function creates the container for the notifications to be render in
-  createContainer : function() {
-      var divContainer = document.createElement("div");
-      divContainer.className = simpleNotify.CONTAINER_CLASS_NAME;
-      divContainer.id = simpleNotify.CONTAINER_ID_NAME;
-      document.body.appendChild(divContainer);
-  },
+	// This function creates the container for the notifications to be render in
+	createContainer: function () {
+		var divContainer = document.createElement("div");
+		divContainer.className = simpleNotify.CONTAINER_CLASS_NAME;
+		divContainer.id = simpleNotify.CONTAINER_ID_NAME;
+		document.body.appendChild(divContainer);
+	},
 
-  showNotification : function(notification) {
-      // Create the new notification element
-      var newNotification = document.createElement("div");
-      newNotification.className = simpleNotify.NOTIFICATION_CLASS_NAME + " " + notification.level;
-      newNotification.id = notification.id;
-      newNotification.innerHTML = notification.message + "<div class='close-notification' onclick='simpleNotify.close(" + newNotification.id + ")'></div>";
-      newNotification.style.marginLeft = simpleNotify.NOTIFICATION_WIDTH + 10 + "px";
-      // Create a wrapper for the notification element so that we can transition it nicely.
-      var notificationWrapper = document.createElement("div");
-      notificationWrapper.className = "simple-notification-wrapper";
-      notificationWrapper.appendChild(newNotification);
-      // Add it to the DOM
-      var notificationContainer = document.getElementById(simpleNotify.CONTAINER_ID_NAME);
-      notificationContainer.insertBefore(notificationWrapper, notificationContainer.firstChild);
+	showNotification: function (notification) {
+		// Create the new notification element
+		var newNotification = document.createElement("div");
+		newNotification.className = simpleNotify.NOTIFICATION_CLASS_NAME + " " + notification.level;
+		newNotification.id = notification.id;
+		newNotification.innerHTML = notification.message + "<div class='close-notification' onclick='simpleNotify.close(" + newNotification.id + ")'></div>";
+		newNotification.style.marginLeft = simpleNotify.NOTIFICATION_WIDTH + 10 + "px";
+		// Create a wrapper for the notification element so that we can transition it nicely.
+		var notificationWrapper = document.createElement("div");
+		notificationWrapper.className = "simple-notification-wrapper";
+		notificationWrapper.appendChild(newNotification);
+		// Add it to the DOM
+		var notificationContainer = document.getElementById(simpleNotify.CONTAINER_ID_NAME);
+		notificationContainer.insertBefore(notificationWrapper, notificationContainer.firstChild);
+		// if auto_dismissing equal true
+		if (simpleNotify.AUTO_DISMISSING) {			
+			// Destroy the notification after the set time
+			setTimeout(function (){
+				simpleNotify.removeNotification(newNotification);
+			}, simpleNotify.NOTIFICATION_TIME);
+		}
+	},
 
-      // Destroy the notification after the set time
-      setTimeout(function() {
-        simpleNotify.removeNotification(newNotification);
-      }, simpleNotify.NOTIFICATION_TIME);
-  },
+	close: function (notification) {
+		simpleNotify.removeNotification(notification);
+	},
 
-  close : function(notification) {
-    simpleNotify.removeNotification(notification);
-  },
-
-  removeNotification : function(notificationToRemove) {
-    notificationToRemove.className = notificationToRemove.className + " fade-out";
-    // Remove the notification from the DOM after the fade out has finished
-    notificationToRemove.addEventListener("transitionend",function(){
-        document.getElementById(simpleNotify.CONTAINER_ID_NAME).removeChild(notificationToRemove.parentElement);
-    },false);
-  }
+	removeNotification: function (notificationToRemove) {
+		notificationToRemove.className = notificationToRemove.className + " fade-out";
+		// Remove the notification from the DOM after the fade out has finished
+		notificationToRemove.addEventListener("transitionend", function () {
+			document.getElementById(simpleNotify.CONTAINER_ID_NAME).removeChild(notificationToRemove.parentElement);
+		}, false);
+	}
 }
